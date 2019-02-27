f5efa3ee838ea4750849096c4176c32eef15e15a
Suresh Kumar E <sureshkumar_e@photonint.com>
2019-02-27 12:00:00 +0530

    added the code

--- welcome/server.js
+++ welcome/server.js
@@ -10,5 +10,38 @@ res.end('Hi Photon');
 
 
 server.listen(port,hostname,function () {
-    
+    var finalValue=[];
+    var a = ["121719", "121720", "121721"];
+    var b = [{
+      empId:"121719",
+      role:"Software Engineer",
+      city:"Dharmapuri",
+      name:"Metikala Suresh"
+    },
+    {
+      empId:"121720",
+      role:"Software Engineer",
+      city:"Tirunelveli",
+      name:"Lakshmanan"
+    },
+    {
+      empId:"121721",
+      role:"Software Engineer",
+      city:"AP",
+      name:"Manjunath"
+    }]
+    function f1(vlaue , callback){
+a.forEach(element =>{
+    f2(element);
+})
+    }
+function f2(value){
+    b.forEach(element =>{
+        if(element.empId==value){
+finalValue.push(element);
+        }
+    })
+}
+    f1(a, f2);
+    console.log(JSON.stringify(finalValue));
 });
